7a2df954e5740c3b8def3550d24f6ae19f26ded3
dhruvidoshi2816 <12402080601034@adit.ac.in>
2026-05-21 11:54:27 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node.js not browser
+
+console.log(3
+    +
+    3) //code readability should be high 
+
+console.log("Dhruvi");
+
+let name = "Dhruvi"
+let age = 20
+let isLoggedIn = true
+
+//"dtatypes"//
+
+//number => range 2 to th power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefine =>
+//symbol => used for uniqueness
+
+
+//object
+
+console.log(typeof "Dhruvi") //string
+console.log(typeof age)  //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
