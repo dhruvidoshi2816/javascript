6fdd101092468953f4479883cf1da6485ec1105c
dhruvidoshi2816 <12402080601034@adit.ac.in>
2026-05-21 12:28:24 +0000

    commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let score = "33abc" //same for undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let scor = null
+
+console.log(typeof scor);
+console.log(typeof(scor));
+
+let dvalueInNumber = Number(scor)
+console.log(typeof dvalueInNumber);
+console.log(dvalueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true =>1, false =>0
+
+
+let isLoggedIn = 1
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);
+
+let LoggedIn = "Dhruvi"
+let BooleanLoggedIn = Boolean(LoggedIn)
+console.log(BooleanLoggedIn);
+
+let aLoggedIn = ""
+let BooleanaLoggedIn = Boolean(aLoggedIn)
+console.log(BooleanaLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Dhruvi" => true
+
+
+
+
