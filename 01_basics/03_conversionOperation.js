a3fa2a95c9e22014cebd39d58c699e640e256af9
dhruvidoshi2816 <12402080601034@adit.ac.in>
2026-05-22 11:13:09 +0000

    commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,46 +1,96 @@
-// let score = 33
+// // let score = 33
+
+// // console.log(typeof score);
+// // console.log(typeof(score));
+
+// let score = "33abc" //same for undefined
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let score = "33abc" //same for undefined
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-console.log(typeof score);
-console.log(typeof(score));
+// let scor = null
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof scor);
+// console.log(typeof(scor));
 
-let scor = null
+// let dvalueInNumber = Number(scor)
+// console.log(typeof dvalueInNumber);
+// console.log(dvalueInNumber);
 
-console.log(typeof scor);
-console.log(typeof(scor));
-
-let dvalueInNumber = Number(scor)
-console.log(typeof dvalueInNumber);
-console.log(dvalueInNumber);
-
-//"33" => 33
-//"33abc" => NaN
-// true =>1, false =>0
+// //"33" => 33
+// //"33abc" => NaN
+// // true =>1, false =>0
 
 
-let isLoggedIn = 1
-let BooleanisLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanisLoggedIn);
+// let isLoggedIn = 1
+// let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn);
 
-let LoggedIn = "Dhruvi"
-let BooleanLoggedIn = Boolean(LoggedIn)
-console.log(BooleanLoggedIn);
+// let LoggedIn = "Dhruvi"
+// let BooleanLoggedIn = Boolean(LoggedIn)
+// console.log(BooleanLoggedIn);
 
-let aLoggedIn = ""
-let BooleanaLoggedIn = Boolean(aLoggedIn)
-console.log(BooleanaLoggedIn);
+// let aLoggedIn = ""
+// let BooleanaLoggedIn = Boolean(aLoggedIn)
+// console.log(BooleanaLoggedIn);
 
-// 1 => true; 0=> false
-// "" => false
-// "Dhruvi" => true
+// // 1 => true; 0=> false
+// // "" => false
+// // "Dhruvi" => true
+
+
+// // ************ Operations *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) //power
+// console.log(2/2)
+// console.log(2%2) //remainder
+
+let str1 = "hello"
+let str2 = " Dhruvi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(3+4*5%3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++; //++gameCounter (prefix and postfix operators)
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
