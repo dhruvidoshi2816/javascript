bebcebd0708f7a513eb276456dd6c1cd80795ae1
dhruvidoshi2816 <12402080601034@adit.ac.in>
2026-05-22 17:40:30 +0000

    go

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++ MATHS ++++++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-4)); //absolute value
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3,6,8));
+// console.log(Math.max(4, 3,6,8));
+
+console.log(Math.random()); //values always between 0 and 1
+console.log(Math.floor(Math.random()*10)+1); //values always between 1 and 10
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.round() * (max-min+1))+min)
